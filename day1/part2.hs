606fb12d8bf84a48b591bfd63f6bf040aa808b38
Martin Maartensson <martinmaartensson@gmail.com>
2023-12-01 12:29:41 +0100

    refactor

--- day1/part2.hs
+++ day1/part2.hs
@@ -1,14 +1,13 @@
 import Data.Char (isDigit)
 
+main :: IO()
 main = interact $ show . sum . map f . lines
 
 f :: String -> Int
-f xs = read $ fst ++ lst
+f xs = read $ head x : [last x]
       where x   = run xs
-            fst = show . head $ x
-            lst = show . last $ x
 
-run :: String -> [Int]
+run :: String -> String
 run [] = []
 run xs = f 0
   where f n 
@@ -17,26 +16,26 @@ run xs = f 0
           where f' Nothing  = f (n+1)
                 f' (Just v) = v : f (n+1)
 
-checkNum :: String -> Maybe Int
+checkNum :: String -> Maybe Char
 checkNum [] = Nothing
 checkNum (x:xs)
-  | isDigit x = Just . read $ [x]
+  | isDigit x = Just x
   | otherwise = f . length $ xs
   where f 0 = Nothing
         f n = f' . parse . take (n+1) $ (x:xs)
           where f' Nothing  = f (n-1)
                 f' (Just v) = Just v
 
-parse :: String -> Maybe Int
-parse s 
-  | s == "one"    = Just 1
-  | s == "two"    = Just 2
-  | s == "three"  = Just 3
-  | s == "four"   = Just 4
-  | s == "five"   = Just 5
-  | s == "six"    = Just 6
-  | s == "seven"  = Just 7
-  | s == "eight"  = Just 8
-  | s == "nine"   = Just 9
-  | otherwise     = Nothing
+
+parse :: String -> Maybe Char
+parse "one"   = Just '1'
+parse "two"   = Just '2'
+parse "three" = Just '3'
+parse "four"  = Just '4'
+parse "five"  = Just '5'
+parse "six"   = Just '6'
+parse "seven" = Just '7'
+parse "eight" = Just '8'
+parse "nine"  = Just '9'
+parse _       = Nothing
 
