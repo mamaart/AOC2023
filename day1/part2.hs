a160802df0d1bc1b6bde62fa2b194d0b57226a12
Martin Maartensson <martinmaartensson@gmail.com>
2023-12-01 16:06:56 +0100

    make more consise

--- day1/part2.hs
+++ day1/part2.hs
@@ -1,33 +1,18 @@
 import Data.Char (isDigit)
+import Data.Maybe (mapMaybe)
 
 main :: IO()
-main = interact $ show . work . parse
+main = interact $ show . work . parse 
 
 work :: [String] -> Int
 work = sum . map (\x -> read $ head x : [last x])
 
 parse :: String -> [String]
-parse = map getNumbers . lines
+parse = map (concatMap getNum . trav) . lines
 
-getNumbers :: String -> String
-getNumbers [] = []
-getNumbers xs = f 0
-  where f n 
-          | n <= length xs = f' . getNumber . drop n $ xs
-          | otherwise      = []
-          where f' Nothing  = f (n+1)
-                f' (Just v) = v : f (n+1)
-
-getNumber :: String -> Maybe Char
-getNumber [] = Nothing
-getNumber (x:xs)
-  | isDigit x = Just x
-  | otherwise = f . length $ xs
-  where f 0 = Nothing
-        f n = f' . convert . take (n+1) $ (x:xs)
-          where f' Nothing  = f (n-1)
-                f' (Just v) = Just v
-
+getNum :: String -> String
+getNum [] = []
+getNum (x:xs) = if isDigit x then [x] else mapMaybe convert . revtrav $ (x:xs)
 
 convert :: String -> Maybe Char
 convert "one"   = Just '1'
@@ -41,3 +26,8 @@ convert "eight" = Just '8'
 convert "nine"  = Just '9'
 convert _       = Nothing
 
+revtrav :: [Char] -> [[Char]]
+revtrav = map reverse . trav . reverse
+
+trav :: [Char] -> [[Char]]
+trav = foldr (\c acc -> case acc of [] -> [[c]]; (x:xs) -> (c : x) : acc) []
