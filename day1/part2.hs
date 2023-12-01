fb9ac624ecb700f4e21210ee449c1d28814816e6
Martin Maartensson <martinmaartensson@gmail.com>
2023-12-01 12:37:09 +0100

    refactor

--- day1/part2.hs
+++ day1/part2.hs
@@ -1,41 +1,43 @@
 import Data.Char (isDigit)
 
 main :: IO()
-main = interact $ show . sum . map f . lines
+main = interact $ show . work . parse
 
-f :: String -> Int
-f xs = read $ head x : [last x]
-      where x   = run xs
+work :: [String] -> Int
+work = sum . map (\x -> read $ head x : [last x])
 
-run :: String -> String
-run [] = []
-run xs = f 0
+parse :: String -> [String]
+parse = map getNumbers . lines
+
+getNumbers :: String -> String
+getNumbers [] = []
+getNumbers xs = f 0
   where f n 
-          | n <= length xs = f' . checkNum . drop n $ xs
+          | n <= length xs = f' . getNumber . drop n $ xs
           | otherwise      = []
           where f' Nothing  = f (n+1)
                 f' (Just v) = v : f (n+1)
 
-checkNum :: String -> Maybe Char
-checkNum [] = Nothing
-checkNum (x:xs)
+getNumber :: String -> Maybe Char
+getNumber [] = Nothing
+getNumber (x:xs)
   | isDigit x = Just x
   | otherwise = f . length $ xs
   where f 0 = Nothing
-        f n = f' . parse . take (n+1) $ (x:xs)
+        f n = f' . convert . take (n+1) $ (x:xs)
           where f' Nothing  = f (n-1)
                 f' (Just v) = Just v
 
 
-parse :: String -> Maybe Char
-parse "one"   = Just '1'
-parse "two"   = Just '2'
-parse "three" = Just '3'
-parse "four"  = Just '4'
-parse "five"  = Just '5'
-parse "six"   = Just '6'
-parse "seven" = Just '7'
-parse "eight" = Just '8'
-parse "nine"  = Just '9'
-parse _       = Nothing
+convert :: String -> Maybe Char
+convert "one"   = Just '1'
+convert "two"   = Just '2'
+convert "three" = Just '3'
+convert "four"  = Just '4'
+convert "five"  = Just '5'
+convert "six"   = Just '6'
+convert "seven" = Just '7'
+convert "eight" = Just '8'
+convert "nine"  = Just '9'
+convert _       = Nothing
 
